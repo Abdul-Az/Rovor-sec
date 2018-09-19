7952c1e712ffe7a6eb90ac87127d77f07d55d5f5
Abdul Azeez <38317160+Abdul-Az@users.noreply.github.com>
2018-09-19 17:38:56 +0530

    added branch login alert

--- src/components/firebase/Userslist.js
+++ src/components/firebase/Userslist.js
@@ -1,36 +1,22 @@
 import React, { Component } from 'react';
 import {firebaseApp} from "./base";
-import {  FormGroup, FormControl, ControlLabel} from "react-bootstrap";
 import { Card, CardImg, CardTitle, CardText, CardDeck,
  CardSubtitle, CardBody } from 'reactstrap';
  import 'bootstrap/dist/css/bootstrap.min.css';
  import {
-    Container, Col, Form, Label, Input,Button 
+    Container, Col, Form,
+    FormGroup, Label, Input,
+    Button,
   } from 'reactstrap';
 
-class Branches extends Component {
+class Userslist extends Component {
     constructor(props) {
         super(props);
-       
-        this.state = {
-            password: ""
-          };
-     this.logout = this.logout.bind(this);
-     this.handleChange = this.handleChange.bind(this);
-     this.handleSubmit = this.handleSubmit.bind(this);
+        this.submit = this.submit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({ password: event.target.value});
-    }
-    handleSubmit(event) {
-        event.preventDefault();
-        this.state.password === "admin" ?
-          alert("matched") : alert(this.state.password)
-    }
-
-    logout() {
-        firebaseApp.auth().signOut();
+    submit() {
+        firebaseApp.auth().submit();
     }
  
 render() {
@@ -41,27 +27,19 @@ render() {
         <CardImg top width="100%"  style={{ width: '400px', height: '400px' }} src={require("../firebase/TCS-01.png" )} alt="Card image cap" />
         <CardBody>
           <CardTitle>TCS Mumbai</CardTitle>
-        <Form  onSubmit={this.handleSubmit} className="form">
+        <Form className="form">
         <Col>
         <FormGroup>
-        {/* <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            /> */}
               <Label for="examplePassword">Password</Label>
               <Input
                 type="password"
                 name="password"
                 id="examplePassword"
                 placeholder="********"
-                onChange={this.handleChange}
               />
             </FormGroup>
           </Col>
-          <Button type="submit" >Submit</Button>
+          <Button>Submit</Button>
         </Form>
         </CardBody>
       </Card>
@@ -71,13 +49,14 @@ render() {
           <CardTitle>TCS Chennai</CardTitle>
         <Form className="form">
         <Col>
-       <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-            //   value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
+        <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="********"
+              />
             </FormGroup>
           </Col>
           <Button>Submit</Button>
@@ -91,4 +70,4 @@ render() {
 }
 }
 // export {Branches};
-export default Branches;
+export default Userslist;
