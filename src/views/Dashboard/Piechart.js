2400d777019534f8227f83537b00311adfca25e4
Abdul Azeez <38317160+Abdul-Az@users.noreply.github.com>
2018-10-22 18:20:04 +0530

    added pie chare dynamic users

--- src/views/Dashboard/Piechart.js
+++ src/views/Dashboard/Piechart.js
@@ -1,9 +1,52 @@
-import React from 'react';
+
 import { Chart } from 'react-chartjs-2';
 import { Container } from 'mdbreact';
+import React,{Component} from 'react';
+
+import firebase from "firebase";
+import {firebaseApp} from "../../components/firebase/base";
+import database from "firebase/database";
+import base from "../../components/firebase/base";
+// import Mumbaiu from "./Mumbaiu";
+// import Chennaiu from "./Chennaiu";
+// import CustomizedTable from "./Userstable";
+import _ from "underscore";
+// import { TimeConverter } from './Timeconverter';
 
 class Piechart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            safe : [],
+            moderate : [],
+            unsafe : [],
+        }
+    }
+    
     componentDidMount() {
+       
+        const UserRef = firebase.database().ref().on("value", function(Data){
+            
+            const tripRoutes = Data.val().tripRoute
+            const tripss = Data.val().trips
+            const tripSpeedss = Data.val().tripSpeeds
+            var userresult = Object.keys(Data.val().Users).map((userId) => {
+                var obj = Data.val().Users[userId]
+                obj.tripRoute = Object.assign({userId},tripRoutes[userId],)
+                obj.trips = Object.assign({userId},tripss[userId],)
+                obj.tripSpeeds = Object.assign({userId},tripSpeedss[userId],)
+                return obj
+            })
+            var notrips = userresult.map(user => {
+                return  Object.keys(user.trips).length - 1; 
+                
+                })
+            this.setState({
+                Allusers : userresult,
+               
+            })
+        }.bind(this)) 
+    
         //doughnut
         var ctxD = document.getElementById("doughnutChart").getContext('2d');
         new Chart(ctxD, {
@@ -12,7 +55,7 @@ class Piechart extends React.Component {
                 labels: ["Unsafe 25%", "Safe 55%", "Moderate 20%"],
                 datasets: [
                     {
-                        data: [3, 8, 4],
+                        data: [30, 80, 40],
                         backgroundColor: ["red", "green", "yellow"],
                         hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
                     }
