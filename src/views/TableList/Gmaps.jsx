e6f22590f517cbca8781492d742a73872ac13861
Abdul Azeez <38317160+Abdul-Az@users.noreply.github.com>
2018-11-03 15:47:15 +0530

    gmaps

--- src/views/TableList/Gmaps.jsx
+++ src/views/TableList/Gmaps.jsx
@@ -1,25 +1,37 @@
 // DoctorsMap.js
-
+/*global google*/
 import React from "react";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, BicyclingLayer, DirectionsRenderer } from "react-google-maps";
 import DoctorMarker from "./GmapsMarker";
+import MapWithADirectionsRenderer from "./GmapsWithDirections";
 
 const DoctorsMap = withScriptjs(withGoogleMap((props) =>{
 
-  // const markers = props.doctors.map( doctor => <DoctorMarker
-  //                   key={doctor.uid}
-  //                   doctor={doctor}
-  //                   location={{lat: doctor.closestPractice.lat, lng: doctor.closestPractice.lon}}
-  //                 />);
-                  
+  // componentDidMount() {
+  //   const DirectionsService = new google.maps.DirectionsService();
+
+  //   DirectionsService.route({
+  //     origin: new google.maps.LatLng(41.8507300, -87.6512600),
+  //     destination: new google.maps.LatLng(41.8525800, -87.6514100),
+  //     travelMode: google.maps.TravelMode.DRIVING,
+  //   }, (result, status) => {
+  //     if (status === google.maps.DirectionsStatus.OK) {
+  //       this.setState({
+  //         directions: result,
+  //       });
+  //     } else {
+  //       console.error(`error fetching directions ${result}`);
+  //     }
+  //   });
+  // }
   return (
       <GoogleMap
         defaultZoom={14}
         center={ { lat:  19.9975, lng: 73.789803 } }
         >
          {<Marker position={{ lat: 19.9975, lng: 73.789803 }} />}
-         <BicyclingLayer autoUpdate />
-         <DirectionsRenderer directions={props.directions} />
+         {/* <BicyclingLayer autoUpdate /> */}
+         <MapWithADirectionsRenderer directions={props.directions} />
         {/* {markers} */}
       </GoogleMap>
     );
